f4a0c865ab56ea7fab3e4f1a19980bcf84513912
Farzin Mohammadi <farzinlize@live.com>
2019-01-21 14:43:28 +0330

    complite bfs kernels

--- kernels.cu
+++ kernels.cu
@@ -1,52 +1,73 @@
-#include "structures.h"
+#include "kernels.h"
 
-__global__ void one_bfs_B_QU(struct graph * g, struct queue * workset, int level)
+__global__ void one_bfs_B_QU(struct graph * g, struct queue * workset, char * update, int level)
 {
-    for (int i = 0; i < workset->size; i++) {
-        int node = workset->items[i];
-        for (int neighbour = g->edge_vector[node]; neighbour < g->edge_vector[node+1]; neighbour_id++) {
-            if (g->node_level_vector[neighbour] > level+1) {
+    int tid = threadIdx.x;
+    int bid = blockIdx.x; 
+    if (bid < workset.size) //each block process a workset entry
+    {
+        int node = workset->items[bid];
+        int neighbours_count = g->edge_vector[node+1] - g->edge_vector[node];
+        if (tid < neighbours_count)
+        {
+            //each thread in block process a neighbour of original node of block
+            int neighbour = g->edge_vector[node] + tid;
+            if (g->node_level_vector[neighbour] > level + 1) {
                 g->node_level_vector[neighbour] = level + 1;
-                queue_push(workset, neighbour);
+                update[neighbour] = 1;
             }
         }
     }
 }
 
-__global__ void one_bfs_B_BM(struct graph * g, char * bitmap, int level)
+__global__ void one_bfs_B_BM(struct graph * g, char * bitmap_mask, char * update, int level)
 {
-    for (int i = 0; i < workset->size; i++) {
-        int node = workset->items[i];
-        for (int neighbour = g->edge_vector[node]; neighbour < g->edge_vector[node+1]; neighbour_id++) {
+    int tid = threadIdx.x;
+    int node = blockIdx.x; //each block process a node
+    if (bitmap_mask[node])
+    {
+        int neighbours_count = g->edge_vector[node+1] - g->edge_vector[node];
+        if (tid < neighbours_count)
+        {
+            //each thread in block process a neighbour of original node of block
+            int neighbour = g->edge_vector[node] + tid;
             if (g->node_level_vector[neighbour] > level+1) {
                 g->node_level_vector[neighbour] = level + 1;
-                queue_push(workset, neighbour);
+                update[neighbour] = 1;
             }
         }
     }
 }
 
-__global__ void one_bfs_T_QU(struct graph * g, struct queue * workset, int level)
+__global__ void one_bfs_T_QU(struct graph * g, struct queue * workset, char * update, int level)
 {
-    for (int i = 0; i < workset->size; i++) {
-        int node = workset->items[i];
-        for (int neighbour = g->edge_vector[node]; neighbour < g->edge_vector[node+1]; neighbour_id++) {
-            if (g->node_level_vector[neighbour] > level+1) {
+    int tid = threadIdx.x;
+    if (tid < workset.size) //each thread process a workset entry
+    {
+        int node = workset->items[tid];
+        //visiting neighbours
+        for (int neighbour = g->edge_vector[node]; neighbour < g->edge_vector[node+1]; neighbour_id++)
+        {
+            if (g->node_level_vector[neighbour] > level+1)
+            {
                 g->node_level_vector[neighbour] = level + 1;
-                queue_push(workset, neighbour);
+                update[neighbour] = 1;
             }
         }
     }
 }
 
-__global__ void one_bfs_T_BM(struct graph * g, struct queue * workset, int level)
+__global__ void one_bfs_T_BM(struct graph * g, char * bitmap_mask, char * update, int level)
 {
-    for (int i = 0; i < workset->size; i++) {
-        int node = workset->items[i];
-        for (int neighbour = g->edge_vector[node]; neighbour < g->edge_vector[node+1]; neighbour_id++) {
+    int node = threadIdx.x;
+    if (bitmap_mask[node]) //each thread process a node if it's in bitmap_mask
+    {
+        //visiting neighbours
+        for (int neighbour = g->edge_vector[node]; neighbour < g->edge_vector[node+1]; neighbour_id++)
+        {
             if (g->node_level_vector[neighbour] > level+1) {
                 g->node_level_vector[neighbour] = level + 1;
-                queue_push(workset, neighbour);
+                update[neighbour] = 1;
             }
         }
     }
---
+++ kernels.h
@@ -0,0 +1,7 @@
+#ifndef _KERNELS_H
+#define _KERNELS_H
+
+#include "structures.h"
+#include <cuda.h>
+
+#endif
