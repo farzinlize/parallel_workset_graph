0a6ccb223b34aa4c9b0077a18f206b19b33f8862
Farzin Mohammadi <farzinlize@live.com>
2019-10-07 17:24:30 +0330

    seperate main and kenrels in linear algebra

---
+++ linear_algebra_main.cuh
@@ -0,0 +1,9 @@
+#ifndef _LINEAR_ALGEBRA_MAIN_H
+#define _LINEAR_ALGEBRA_MAIN_H
+
+#include "linear_algebra.cuh"
+
+void linear_algebra_bfs(graph g_h, int source);
+void linear_algebra_bfs_scalar(graph g_h, int source);
+
+#endif
